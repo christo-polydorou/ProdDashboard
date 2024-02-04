6d046c99b19b01deebd5311b1b69b3686109f0fa
sherrickb <ben.a.sherrick@gmail.com>
2024-02-04 14:38:33 -0600

    edited addTask, added SettingsViews

---
+++ ProdDashboard/Code/Features/Settings/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ProdDashboard
+//
+//  Created by Ben Sherrick on 2/4/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
