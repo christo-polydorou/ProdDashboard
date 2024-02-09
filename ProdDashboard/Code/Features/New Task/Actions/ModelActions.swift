01b6e6e241f2176dcea30b0382b69bc48226cadc
AidanMorris <morrisa4@carleton.edu>
2024-02-09 09:17:11 -0600

    ML functions

--- ProdDashboard/Code/Features/New Task/Actions/ModelActions.swift
+++ ProdDashboard/Code/Features/New Task/Actions/ModelActions.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import CoreML
+
+func getPrediction(name: String, expect: Double, personal: Double) -> Double {
+    let model = MyTabularRegressor_3()
+    do {
+        let modelOutput = try model.prediction(Name: name, Expect: Int64(expect), Personal: Int64(personal))
+        return modelOutput.Real
+    } catch {
+        print("Error getting prediction: \(error)")
+        return -1
+    }
+}
+
+
+func recommendTime(taskName: String, taskExpect: Double, taskPersonal: Double, freeTimes: [DateInterval]) -> Double {
+    // Assume intervals of 15 minutes and only times without other tasks
+    var lowestTime = Double.infinity
+    for interval in freeTimes {
+        var curTime = interval.start
+        while curTime != interval.end {
+            let curPred = getPrediction(name: taskName, expect: taskExpect, personal: taskPersonal)
+            if curPred < lowestTime {
+                var doubleValue = curTime.timeIntervalSinceReferenceDate
+                
+                lowestTime = doubleValue
+            }
+            curTime = curTime.addingTimeInterval(15 * 60)
+        }
+    }
+    return lowestTime
+}
+
+func getTempDate() -> DateInterval {
+    var dateComponentsStart = DateComponents()
+    dateComponentsStart.year = 2024
+    dateComponentsStart.month = 2
+    dateComponentsStart.day = 4
+    dateComponentsStart.hour = 8
+    dateComponentsStart.minute = 0
+    
+    var dateComponentsEnd = DateComponents()
+    dateComponentsEnd.year = 2024
+    dateComponentsEnd.month = 2
+    dateComponentsEnd.day = 4
+    dateComponentsEnd.hour = 20
+    dateComponentsEnd.minute = 0
+    
+    let calendar = Calendar.current
+    
+    if let dateStart = calendar.date(from: dateComponentsStart),
+           let dateEnd = calendar.date(from: dateComponentsEnd) {
+            return DateInterval(start: dateStart, end: dateEnd)
+    } else {
+        return DateInterval()
+    }
+}
+
+
--- ProdDashboard/Code/Features/New Task/Views/RecView.swift
+++ ProdDashboard/Code/Features/New Task/Views/RecView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct RecView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // let result = String(getPrediction(name: "Make Bed", expect: 3, personal: 1))
+        // Text(result)
+        
+        
+        let testInterval = getTempDate()
+        
+        let recommendedStart = recommendTime(taskName: "Laundry", taskExpect: 3, taskPersonal: 1, freeTimes: [testInterval])
+        
+        
+        
+        var formattedTime: String {
+            let timeInterval = TimeInterval(recommendedStart)
+            let referenceDate = Date(timeIntervalSinceReferenceDate: 0)
+            let resultDate = referenceDate.addingTimeInterval(timeInterval)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH:mm:ss"
+            return dateFormatter.string(from: resultDate)
+            
+        }
+        
+        Text(formattedTime)
     }
 }
 
