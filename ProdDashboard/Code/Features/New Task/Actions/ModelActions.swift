15328590554696af97d5a60175bf24e60bbe4e25
AidanMorris <morrisa4@carleton.edu>
2024-02-09 09:15:43 -0600

    ML functions

---
+++ ProdDashboard/Code/Features/New Task/Actions/ModelActions.swift
@@ -0,0 +1,8 @@
+//
+//  ModelActions.swift
+//  ProdDashboard
+//
+//  Created by Aidan Morris on 2/5/24.
+//
+
+import Foundation
---
+++ ProdDashboard/Code/Features/New Task/Views/RecView.swift
@@ -0,0 +1,18 @@
+//
+//  RecView.swift
+//  ProdDashboard
+//
+//  Created by Aidan Morris on 2/5/24.
+//
+
+import SwiftUI
+
+struct RecView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecView()
+}
