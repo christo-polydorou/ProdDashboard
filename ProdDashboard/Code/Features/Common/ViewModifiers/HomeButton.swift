0d8fdf15cc4dbf57b3021ea63fe33680fe1f35a4
Christo <christopolydorou@gmail.com>
2024-02-11 22:21:24 -0600

    Missing files

---
+++ ProdDashboard/Code/Features/Common/ViewModifiers/HomeButton.swift
@@ -0,0 +1,17 @@
+//
+//  HomeButton.swift
+//  ProdDashboard
+//
+//  Created by Aidan Morris on 1/21/24.
+//
+
+import SwiftUI
+
+struct HomeButton: ViewModifier {
+    func body(content: Content) -> some View {
+        VStack {
+            Image(systemName: "house.fill")
+            content
+        }
+    }
+}
