bbd7cc0c4c307280fd7d943d06502cfb823a62ab
AidanMorris <morrisa4@carleton.edu>
2024-02-12 21:34:26 -0600

    work on updating the model, not working code

--- ProdDashboard/Code/Features/New Task/Actions/ModelActions.swift
+++ ProdDashboard/Code/Features/New Task/Actions/ModelActions.swift
@@ -63,4 +63,50 @@ func getTempDate() -> DateInterval {
     }
 }
 
+var updateContext: MLUpdateContext?
+
+
+func updateModel(newTasks: [Task]) -> Void {
+    let bundle = Bundle(for: MyTabularRegressor_3.self)
+    let updatableModelURL = bundle.url(forResource: "MyTabularRegressor_3", withExtension: "mlmodelc")!
+    var featureProviders = [MLFeatureProvider]()
+    let tasknameName = "Name"
+    let expectedName = "Expected"
+    let outputName = "Real"
+    for task in newTasks {
+        let nameValue = MLFeatureValue(string: task.name)
+        let expectedValue = MLFeatureValue(int64: task.expected)
+        let outputName = MLFeatureValue(int64: task.real)
+        let dataPointFeatures: [String: MLFeatureValue] = [tasknameName: nameValue,
+                                                           expectedName: expectedValue,
+                                                           outputName: outputName]
+        if let provider = try? MLDictionaryFeatureProvider(dictionary: dataPointFeatures) {
+            featureProviders.append(provider)
+        }
+    }
+    let batch = MLArrayBatchProvider(array: featureProviders)
+    
+    let config = MLModelConfiguration()
+    let updateTask = try! MLUpdateTask(forModelAt: updatableModelURL, trainingData: batch, configuration: config) { context in
+        self.updateContext = context
+    }
+    
+    updateTask.resume()
+    if let updatedModel = updateContext?.model {
+        // Specify the URL where you want to save the updated model
+        let updatedModelURL = URL(fileURLWithPath: updatableModelURL.absoluteString)
+
+        do {
+            // Save the updated model to the specified URL
+            try updatedModel.write(to: updatedModelURL)
+            print("Updated model saved successfully at: \(updatedModelURL)")
+        } catch {
+            print("Error saving updated model: \(error.localizedDescription)")
+        }
+    } else {
+        print("No updated model available.")
+    }
+    
+}
+
 
