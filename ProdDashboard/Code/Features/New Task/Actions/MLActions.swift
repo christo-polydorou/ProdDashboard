f66c3d3f22d8ce276993bdae9c8f642cd2742d34
AidanMorris <morrisa4@carleton.edu>
2024-02-20 20:17:30 -0600

    ML Functionality

---
+++ ProdDashboard/Code/Features/New Task/Actions/MLActions.swift
@@ -0,0 +1,8 @@
+//
+//  MLActions.swift
+//  ProdDashboard
+//
+//  Created by Aidan Morris on 2/13/24.
+//
+
+import Foundation
---
+++ ProdDashboard/Code/Features/New Task/Actions/NLPActions.swift
@@ -0,0 +1,8 @@
+//
+//  NLPActions.swift
+//  ProdDashboard
+//
+//  Created by Aidan Morris on 2/13/24.
+//
+
+import Foundation
